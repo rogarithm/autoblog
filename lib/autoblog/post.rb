7b90fea89d2d9365df47cb65a1be8c038493173d
sehoon.gim <sehoongim@centerlink.kr>
2024-01-11 00:15:24 +0900

    단순한 내용의 파일을 html로 변환할 수 있다

--- lib/autoblog/post.rb
+++ lib/autoblog/post.rb
@@ -5,7 +5,11 @@ module AutoBlog
     def initialize(file)
       @content = File.read(
         File.join(File.dirname(__FILE__), *%w[.. .. source], file)
-      )
+      ).strip
+    end
+
+    def to_html content
+      "<p>#{content}</p>"
     end
   end
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -4,6 +4,11 @@ describe AutoBlog::Post do
 
   it "원본 글이 있는 곳에서 파일을 읽을 수 있다" do
     p = AutoBlog::Post.new('test.md')
-    expect(p.content).to eq("HI!\n");
+    expect(p.content).to eq("HI!");
+  end
+
+  it "파일을 html로 변환할 수 있다" do
+    p = AutoBlog::Post.new('test.md')
+    expect(p.to_html(p.content)).to eq('<p>HI!</p>');
   end
 end
