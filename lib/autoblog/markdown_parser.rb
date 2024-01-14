48c6aaff5d20b0996afab190d65be4d1c894cb85
sehoon.gim <sehoongim@gmail.com>
2024-01-14 19:18:10 +0900

    헤더를 html로 변환할 수 있다

--- lib/autoblog/markdown_parser.rb
+++ lib/autoblog/markdown_parser.rb
@@ -3,5 +3,14 @@ module AutoBlog
     def convert_paragraph content
       "<p>#{content}</p>"
     end
+    def convert_header1 line
+      line.sub(/^#(.*)/, '<h1>\1</h1>')
+    end
+    def convert_header2 line
+      line.sub(/^##(.*)/, '<h2>\1</h2>')
+    end
+    def convert_header3 line
+      line.sub(/^###(.*)/, '<h3>\1</h3>')
+    end
   end
 end
