aa38ef71820bad899d2ddd7c5f9433c4c35a0304
sehoon.gim <sehoongim@gmail.com>
2024-01-23 16:55:07 +0900

    테스트 실행 시 만든 파일을 정리한다

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../lib/autoblog/site'
 
 describe AutoBlog::Site do
+  def clean_up(path)
+    generated_files = Dir.entries(path).reject! {|entry| entry == "." or entry == ".."}
+    generated_files.each do |f|
+      FileUtils.rm("#{path}/#{f}")
+    end
+  end
 
   context "path 내의 md 파일 모두 post 객체로 만든다" do
     it "path 내의 md 파일 목록을 가져온다" do
@@ -14,5 +20,6 @@ describe AutoBlog::Site do
   it "만든 post 객체를 가지고 dest 디렉토리에 변환된 html 파일을 저장한다" do
       s = AutoBlog::Site.new(File.join(File.dirname(__FILE__), *%w[source]))
       s.process(File.join(File.dirname(__FILE__), *%w[dest]))
+    clean_up(File.join(File.dirname(__FILE__), *%w[dest]))
   end
 end
