2bf3586bbc46cad4e1331909e760ed6ba5f1d27b
sehoon.gim <sehoongim@gmail.com>
2024-02-25 18:32:59 +0900

    matchesPlus를 구현한다
     - 하나 이상의 token occurrence를 매칭하는 역할을 한다.
     - matchesStar와 다르게, 매칭이 되지 않았을 떄는 Node.null을 반환한다.

---
+++ lib/autoblog/converter/parser/parsers/concerns/matches_plus.rb
@@ -0,0 +1,25 @@
+module MatchesPlus
+  # This method tries to match a sentence as many times as possible. It then
+  # returns all matched nodes. It's the equivalent of `*`, also known as Kleene
+  # star.
+  #
+  def match_plus(tokens, with:)
+    matched_nodes = []
+    consumed      = 0
+    parser        = with
+
+    while true
+      node = parser.match(tokens.offset(consumed))
+      break if node.null?
+      matched_nodes += [node]
+      consumed      += node.consumed
+    end
+
+    if consumed == 0
+      Node.null
+    else
+      [matched_nodes, consumed]
+    end
+  end
+end
+
