13392e453b5f6bc9a890bdd8158711f12baedc75
sehun-kim <rogarithm@gmail.com>
2024-07-17 22:33:48 +0900

    깨지는 테스트 고친다

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -6,7 +6,11 @@ describe AutoBlog::Post do
   end
 
   it "파일을 html로 변환할 수 있다" do
-    expect(@p.to_html()).to eq('<p>HI!</p>')
+    expect(@p.to_html()).to eq(
+'<p>
+  HI!
+</p>
+')
   end
 
   it "html로 변환된 파일을 저장할 수 있다" do
