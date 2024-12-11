6644bb7a3ad379d2c52f3f37ba2c291ba5686e83
sehoon.gim <sehoongim@gmail.com>
2024-12-11 15:45:22 +0900

    정적 파일 처리 오류를 고친다
     - dest/css, dest/font 디렉토리 안으로 정적 파일을 옮길 때
       dest/font/font 처럼 디렉토리가 하나 더 생기는 오류가 있었다
     - FileUtils.cp_r의 인자를 바꿔서 해당 케이스가 발생하지 않도록 했다
     - FileUtils.mkdir_p로 디렉토리가 만들어지지 않았을 경우 만들도록 한다
       (내부 구현에서 이미 생성된 디렉토리인 경우 새로 만들지 않도록
       처리하고 있다)

--- spec/helpers/file_helper.rb
+++ spec/helpers/file_helper.rb
@@ -2,7 +2,7 @@ module FileHelper
   def clean_up(path)
     generated_files = Dir.entries(path).reject! {|entry| entry == "." or entry == ".."}
     generated_files.each do |f|
-      FileUtils.rm("#{path}/#{f}")
+      FileUtils.rm "#{path}/#{f}"
     end
   end
 end
